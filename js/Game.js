caff0b326060d749b844e7dd717f4c8291b98c74
ArnavNawal <snawal34@gmail.com>
2020-12-25 21:25:39 +0530

    carracing

--- js/Game.js
+++ js/Game.js
@@ -44,7 +44,7 @@ class Game {
     form.hide();
     
     Player.getPlayerInfo();
-    
+    player.GetCarsAtEnd();
     if(allPlayers !== undefined){
       background(rgb(198,135,103));
       image(track, 0,-displayHeight*4,displayWidth, displayHeight*5);
@@ -91,8 +91,10 @@ class Game {
       player.update();
     }
 
-    if(player.distance > 3860){
+    if(player.distance > 5200){
       gameState = 2;
+      player.rank = player.rank+1
+      Player.UpdateCarsAtEnd(player.rank);
     }
    
     drawSprites();
@@ -100,5 +102,6 @@ class Game {
 
   end(){
     console.log("Game Ended");
+    console.log(player.rank);
   }
 }
